1917ccea8812c029321f0b875efdcc5dcfad2b9b
Ben Brown <benbrown@gmail.com>
2024-11-22 16:45:08 -0600

    add polyfill for node modules

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { execSync } from "node:child_process";
+import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill'
 
 let hash = "";
 
@@ -11,7 +12,7 @@ try {
 }
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(),pluginNodePolyfill()],
   source: {
     define: {
       "process.env.COMMIT_HASH": JSON.stringify(hash),
